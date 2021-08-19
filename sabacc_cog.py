2b5295bef30c0fa20db4af7ab88b37ee5078e810
Emily <captain.emily.blum@gmail.com>
2021-08-19 10:19:00 -0400

    Continue to tweak join command, make sure methods are properly async so everything can work.

--- sabacc_cog.py
+++ sabacc_cog.py
@@ -11,45 +11,51 @@ class sabacc(commands.Cog):
     self.games = {}
 
   @commands.command(name="setup-account")
-  def acct_setup(self,ctx):
+  async def acct_setup(self,ctx):
     acct = database.select_one(dbobj.accounts, id=ctx.author.id)
     if acct == [] or acct == None:
       database.insert_row(dbobj.accounts, (ctx.author.id,100))
-      ctx.send("Your account has been initialized to 100 credits. If you want more, plead with a moderator about it I guess. See what happens.")
+      await ctx.send("Your account has been initialized to 100 credits. If you want more, plead with a moderator about it I guess. See what happens.")
     else:
-      ctx.send("You may already have an account. If you believe this to be untrue, check with the owner of the bot or a moderator.")
+      await ctx.send("You may already have an account. If you believe this to be untrue, check with the owner of the bot or a moderator.")
 
   @commands.command(name="setup-game",help="Start a game that people can join and then play. Also automatically adds you to the game.")
-  def game_setup(self,ctx):
+  async def game_setup(self,ctx):
     acct = database.select_one(dbobj.accounts, id=ctx.author.id)
     if acct == [] or acct[1] == 0:
-      ctx.send("You have no credits! If this is because you do not have an account, make one with the `create-account` command. If this is because you have bad luck... maybe ask a moderator if you can have a bit more money?",delete_after=7)
+      await ctx.send("You have no credits! If this is because you do not have an account, make one with the `create-account` command. If this is because you have bad luck... maybe ask a moderator if you can have a bit more money?",delete_after=7)
       return
     try:
       if self.games[ctx.channel.id] != {}:
-        ctx.send("There is either already a game running here in this channel, or the bot is broken. Sorry if it's the latter.",delete_after=5)
+        await ctx.send("There is either already a game running here in this channel, or the bot is broken. Sorry if it's the latter.",delete_after=5)
     except:
       pass
     self.games[ctx.channel.id] = {}
     self.games[ctx.channel.id]["players"] = []
     self.games[ctx.channel.id]["players"].add(ctx.author.id)
     self.games[ctx.channel.id]["deck"] = Deck()
-    ctx.send("Game setup complete! Now up to five more people can join using the `join` command!")
+    msg = await ctx.send("Game setup complete! Now up to five more people can join using the `join-game` command! (1/6 Players Joined)")
+    self.games[ctx.channel.id]["msg"] = msg.id
 
   @commands.command(name="join-game",help="Join a game that is about to be started. Must be run in a channel where a game is being setup.")
-  def game_join(self,ctx):
+  async def game_join(self,ctx):
     try:
       if self.games[ctx.channel.id] != {}:
         acct = database.select_one(dbobj.accounts, id=ctx.author.id)
         if acct == [] or acct[1] == 0:
-          ctx.send("You have no credits! If this is because you do not have an account, make one with the `create-account` command. If this is because you have bad luck... maybe ask a moderator if you can have a bit more money?",delete_after=7)
+          await ctx.send("You have no credits! If this is because you do not have an account, make one with the `create-account` command. If this is because you have bad luck... maybe ask a moderator if you can have a bit more money?",delete_after=7)
+          return
+        if len(self.games[ctx.channel.id]["players"]) >= 6:
+          await ctx.send("Sorry, this game is full at the moment.",delete_after=5)
           return
         self.games[ctx.channel.id]["players"].add(ctx.author.id)
-        ctx.send("You were successfully added to the game!",delete_after=5)
+        await ctx.send("You were successfully added to the game!",delete_after=5)
+        msg = await ctx.channel.fetch_message(self.games[ctx.channel.id]["msg"])
+        await msg.edit("Game setup complete! Now up to five more people can join using the `join-game` command! ({0}/6 Players Joined)".format(len(self.games[ctx.channel.id]["players"])))
       else:
-        ctx.send("There is no game available to join in this channel!",delete_after=5)
+        await ctx.send("There is no game available to join in this channel!",delete_after=5)
     except:
-      ctx.send("There is no game available to join in this channel!",delete_after=5)
+      await ctx.send("There is no game available to join in this channel!",delete_after=5)
 
 
 class Card():
